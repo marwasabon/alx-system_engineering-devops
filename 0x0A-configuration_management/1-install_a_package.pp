458902ec9e66dbefe33824a6ba423156f6c13dfd
marwasabon <marwasabon@gmail.com>
2023-12-21 10:36:10 +0000

    task 1.1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 # install flask from pip3.
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
-}
+package { 'pip3':
+	ensure   => '2.1.0',
+	provider => 'flask',
+	}
+#exec { 'install_flask':
+#  command => '/usr/bin/pip3 install Flask==2.1.0',
+#  path    => ['/usr/bin'],
+#}
