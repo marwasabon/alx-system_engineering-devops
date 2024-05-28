0a00d27f5e59470557bb7a205117335efa67c210
marwasabon <marwasabon@gmail.com>
2024-05-28 13:05:08 +0000

    task 0

---
+++ 0x14-mysql/install_mysql.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update
+
+# Install MySQL Server
+sudo apt install -y mysql-server
+
+# Secure MySQL installation (optional: Configure root password, remove anonymous users, etc.)
+# sudo mysql_secure_installation
