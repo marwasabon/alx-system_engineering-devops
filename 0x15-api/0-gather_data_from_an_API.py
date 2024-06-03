44d7a3090d5128002c131a3e501cd039be1f42a6
marwasabon <marwasabon@gmail.com>
2024-06-03 02:26:53 +0000

    task 0.0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python script to returns information about his/her TODO list progress"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    entrypoint = "https://jsonplaceholder.typicode.com"
+    usrId = sys.argv[1]
+    try:
+        user_req = requests.get(
+            "{ep}/users/{usrId}".format(ep=entrypoint, usrId=usrId)
+        ).json()
+        if user_req:
+            tasks = requests.get(
+                "{ep}/todos".format(ep=entrypoint),
+                params={"userId": usrId}
+            ).json()
+            num_of_tasks = len(tasks)
+            finished_tasks = [t for t in tasks if t['completed'] is True]
+            print(
+                "Employee {} is done with tasks({}/{}):"
+                .format(user_req['name'], len(finished_tasks), num_of_tasks)
+            )
+            [print('\t ' + task['title']) for task in finished_tasks]
+    except Exception as e:
+        print(e)
