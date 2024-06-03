f9b93e5a46b55aa1507a214419df4586369974ed
marwasabon <marwasabon@gmail.com>
2024-06-03 03:02:45 +0000

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Python script to returns information 
+about his/her TODO list progress convert the data
+to a CSV
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    import csv
+
+    entrypoint = "https://jsonplaceholder.typicode.com"
+    usrId = sys.argv[1]
+    user_req = requests.get(
+            "{ep}/users/{usrId}".format(ep=entrypoint, usrId=usrId)
+            ).json()
+    if user_req:
+        tasks = requests.get(
+                "{ep}/todos".format(ep=entrypoint),
+                params={"userId": usrId}
+                ).json()
+        with open("{usrId}.csv".format(usrId=usrId), "w+") as f:
+            fields = ("id", "username", "status", "title")
+            writer = csv.DictWriter(
+                    f,
+                    fieldnames=fields,
+                    lineterminator='\n',
+                    quotechar='"',
+                    quoting=csv.QUOTE_ALL
+                    )
+            for task in tasks:
+                writer.writerow(dict(
+                id=task['userId'],
+                username=user_req["username"],
+                status=task["completed"],
+                title=task["title"]
+                ))
+
