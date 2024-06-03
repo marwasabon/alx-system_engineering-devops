08ec38171f6b3beb84e06f85d7035a76230b8e7d
marwasabon <marwasabon@gmail.com>
2024-06-03 08:33:49 +0000

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Python script to returns information about his/her TODO list progress"""
+import json
+import requests
+
+
+if __name__ == "__main__":
+
+    entrypoint = "https://jsonplaceholder.typicode.com"
+    users = requests.get("{}/users".format(entrypoint)).json()
+    tasks = requests.get("{}/todos".format(entrypoint)).json()
+
+    with open("todo_all_employees.json", "w+") as f:
+        collected_tasks = dict()
+        for usr in users:
+            usr_id = usr["id"]
+            usr_task = {usr_id: []}
+            for t in [t for t in tasks if t['userId'] == usr_id]:
+                task = dict(
+                        username=usr["username"],
+                        task=t["title"],
+                        completed=t["completed"]
+                        )
+                tasks.remove(t)
+                usr_task[usr_id].append(task)
+                collected_tasks.update(usr_task)
+                json.dump(collected_tasks, f)
