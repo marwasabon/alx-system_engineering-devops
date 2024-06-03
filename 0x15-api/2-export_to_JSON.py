cb9527f00cae2e68ccd5d2288eaf20e834689666
marwasabon <marwasabon@gmail.com>
2024-06-03 03:14:52 +0000

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Python script to returns information
+about his/her TODO list progress export data in the JSON format.
+"""
+
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+    import csv
+
+    entrypoint = "https://jsonplaceholder.typicode.com"
+    usrId = sys.argv[1]
+    user_req = requests.get(
+            "{ep}/users/{usrId}".format(ep=entrypoint, usrId=usrId)
+            ).json()
+    if user_req:
+        tasks = requests.get(
+                "{ep}/todos".format(ep=entrypoint),
+                params={"userId": usrId}
+                ).json()
+        with open("{}.json".format(usrId), "w+") as f:
+            # someway to get the id
+            usr_task = {usrId: []}
+            for task in tasks:
+                t_d = dict(
+                        username=user_req["username"],
+                        task=task["title"],
+                        completed=task["completed"]
+                        )
+                usr_task[usrId].append(t_d)
+                json.dump(usr_task, f)
