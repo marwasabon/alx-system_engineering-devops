28d050ad427095a470c8d909410fefc32d85dc9b
marwasabon <marwasabon@gmail.com>
2024-06-03 03:06:03 +0000

    task 1.2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Python script to returns information 
+"""Python script to returns information
 about his/her TODO list progress convert the data
 to a CSV
 """
+
 if __name__ == "__main__":
     import requests
     import sys
@@ -29,9 +30,8 @@ if __name__ == "__main__":
                     )
             for task in tasks:
                 writer.writerow(dict(
-                id=task['userId'],
-                username=user_req["username"],
-                status=task["completed"],
-                title=task["title"]
+                    id=task['userId'],
+                    username=user_req["username"],
+                    status=task["completed"],
+                    title=task["title"]
                 ))
-
