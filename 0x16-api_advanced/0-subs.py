183b74d4fe3c45893e8078cc6eb67cfba6a073c7
marwasabon <marwasabon@gmail.com>
2024-06-18 03:16:49 +0000

    task 00001

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,15 +5,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Function that returns  the number of users subscribed to reddit"""
-    try:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        request = requests.get(url, headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    res = requests.get(url, headers, allow_redirects=False)
+    if res.status_code == 200:
+        data = res.json()
+        subscribers = data['data']['subscribers']
         return subscribers
-        else:
-            return 0
-    except Exception as e:
+    else:
         return 0
