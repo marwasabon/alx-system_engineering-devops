6e2141c6cc989ac4b182e82fac1771be8b8d9375
marwasabon <marwasabon@gmail.com>
2024-06-18 03:09:03 +0000

    task 000

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Python scripts that retruns the number of subscribers"""
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
@@ -10,6 +9,6 @@ def number_of_subscribers(subreddit):
         url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
         headers = {'User-Agent': 'Mozilla/5.0'}
         request = requests.get(url, headers, allow_redirects=False)
-        return request.json().get('data').get('subscribers')
+        return request.json().get('data').get('subscribers',0)
     except Exception as e:
         return 0
